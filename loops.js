d05c65e3d21991c19a49e49d0333b8e647385721
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-22 23:50:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
   for(let i = 0; i < 25; i++) {
     if(i === 1) {
-      array[array.length + i - 12] = "I am 1 strange loop."
+      array[array.length + parseInt(i, 10)] = "I am 1 strange loop."
     }
-    else { array[array.length + i - 12] = "I am ${i} strange loops."
+    else { array[array.length + parseInt(i, 10)] = "I am ${i} strange loops."
     }
   }
 
