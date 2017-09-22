43538a1c4e2a0f9e74b46b1c09963d017373f0fc
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-22 23:15:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
-function forLoop(array){
-  for(var i = 0, i < 25, i++) {
+function forLoop(array) {
+  for(var i = 0; i < 25; i++) {
     if(i === 1) {
       array[array.length + i] = "I am 1 strange loop."
     }
-    else array[array.length + i] = "I am ${ i } strange loops."
+    else array[array.length + i] = "I am ${i} strange loops."
   }
 
   return array
